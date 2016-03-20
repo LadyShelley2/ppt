29aa1086af817ae875f40ace53e93deecddf3172
zhangle <810881283@qq.com>
2016-03-21 00:09:21 +0800

    new v

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var nodePPT=require('nodeppt/lib/nodePPT.js');
 var ipv4 = require('ipv4');
 
 var cmd={
-	host:process.env.OPENSHIFT_NODEJS_IP || process.env.NODE_PPT_HOST || ipv4,
+	host:process.env.OPENSHIFT_NODEJS_IP || process.env.NODE_PPT_HOST || ipv4 || '127.0.0.1',
 	port: process.env.OPENSHIFT_NODEJS_PORT || process.env.NODE_PPT_PORT ||'8080',
 	socket:'support websocket mutil screen controller',
 	watch:'livereload',
