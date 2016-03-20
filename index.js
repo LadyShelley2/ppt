709aa55a56ec178ea1f3e4c9433f84622d415aac
zhangle <810881283@qq.com>
2016-03-20 19:34:52 +0800

    fix host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var nodePPT=require('nodeppt/lib/nodePPT.js');
 var ipv4 = require('ipv4');
 
 var cmd={
-	host:process.env.OPENSHIFT_NODEJS_IP || ipv4 || '0.0.0.0',
+	host:process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 	port: process.env.OPENSHIFT_NODEJS_PORT || '8080',
 	socket:'support websocket mutil screen controller',
 	watch:'livereload',
